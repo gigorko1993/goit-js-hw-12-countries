215637a7b49892c9b696bd4091f444669190ab28
gigorko1993 <gigorko1993@gmail.com>
2021-05-28 16:40:42 +0300

    create markup wiht 1 and 1-10 country

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 import './sass/main.scss';
 import countryCardTpl from '../src/templates/country-list.hbs'
+import countryCardsTpl from '../src/templates/countries-list.hbs'
+
+const debounce = require("lodash.debounce");
 
 const refs = {
     cardContainer: document.querySelector('.js-country__container'),
     input: document.querySelector('.search-country'),
 }
-refs.input.addEventListener('input', onSearch)
+const { cardContainer, input } = refs;
+input.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(ev) {
     ev.preventDefault();
 
-    const inputedText = ev.currentTarget.value;
+    const inputedText = ev.target.value;
     fetchCountry(inputedText)
-    .then(renderCountryCard)
-    .catch(error => console.log(error))
+        .then(renderCountryCard)
+        .catch(error => console.log(error));
 }
-
 
 function fetchCountry(name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
         .then(res => { return res.json() })
-}
+        }
 
-    function renderCountryCard(country) {
-        console.log(country);
+function renderCountryCard(country) {
+    if (country.length === 1) {
         const markup = countryCardTpl(country);
-        refs.cardContainer.innerHTML = markup;
-        console.log(markup);
+        cardContainer.innerHTML = markup;
+    } else if (country.length > 1 && country.length < 11) {
+        const markup = countryCardsTpl(country);
+        cardContainer.innerHTML = markup;
+    } 
     }
